a4f42e861d47984fe29645cfa111fd43f5bf7435
Rania Fatnassi <rania.fatnassi12@gmail.com>
2020-08-11 22:46:38 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var pets = [
+    { name: "Max", type: "dog", bornOn: 2018 },
+    { name: "Angel", type: "cat", bornOn: 2015 },
+    { name: "Jasper", type: "dog", bornOn: 2016 }
+];
+
+
+const getAge = pet => new Date().getFullYear() - pet.bornOn; 
+
+pets.filter((pet)=>{
+    console.log(pet.getAge(pet));
+})
+
+pets.filter((pet)=>{
+    console.log(pet.type == "dog");
+})
+
+people.find(person=>{return person.name == name});
+
+let jasper;
+jasper = pets.find(pet=>{pet.name == 'Jasper'});
+console.log(`Jasper is ${jasper.age} years old`);
